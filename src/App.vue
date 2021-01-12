7958527a12260051c27bfd891ce0241a4b123606
MarcPrrn-hub <marcperrin98@gmail.com>
2021-01-12 18:44:20 +0100

    avancées

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
 
     <section class="main-content w-50 mx-auto">
       <main-navigation></main-navigation>
-      <windows-list></windows-list>
-      <rooms-list></rooms-list>
+      <windows-list ></windows-list>
+      <rooms-list ></rooms-list>
     </section>
   </div>
 </template>
--- src/components/MainNavigation.vue
+++ src/components/MainNavigation.vue
@@ -1,17 +1,36 @@
 <template>
   <ul class="nav nav-tabs">
     <li class="nav-item">
-      <a class="nav-link active" aria-current="page" href="#">Windows</a>
+      <button class="nav-link me-2" href="#" @click="change_tab_window" >Windows</button>
     </li>
     <li class="nav-item">
-      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" >Rooms</a>
+      <button type="button" class="nav-link" href="#"  @click="change_tab_room">Rooms</button>
     </li>
   </ul>
 </template>
 
-
+//changer les fonctions pour que tab_window et tab_room puisse etre récup dans App pour l'affichage conditionel
 <script>
 export default {
   name: 'MainNavigation',
+  props: {
+    tab_window: Boolean,
+    tab_room: Boolean,
+  },
+  methods: {
+    change_tab_room() {
+      this.tab_window = false;
+      this.tab_room = true;
+      console.log(this.tab_window)
+      console.log(this.tab_room)
+
+    },
+    change_tab_window() {
+      this.tab_window = true;
+      this.tab_room = false;
+      console.log(this.tab_window)
+      console.log(this.tab_room)
+    }
+  }
 }
 </script>
